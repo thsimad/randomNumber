4364203a05f88bc4dc0f1dc498df2ce0d140f6a3
Syed Imaduddin Ahmed <erimadahmad@gmail.com>
2018-09-12 18:54:02 +0530

    v1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,14 +7,14 @@ function(req, res){
 });
 //linked auth callback
 router.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
-    successRedirect: '/',
+    successRedirect: '/form',
     failureRedirect: '/login'
 }));
 //logout route
 router.get('/logout', (req, res) => {
     req.logout();
     req.flash('success', 'Successfully logged out!');
-    res.redirect('/');
+    res.redirect('/register');
 });
 
         
