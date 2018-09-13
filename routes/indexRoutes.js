e589c9fd9dbfbd8049d3c88a802b5f6c7238026e
Syed Imaduddin Ahmed <erimadahmad@gmail.com>
2018-09-14 01:14:28 +0530

    v1

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -6,8 +6,8 @@ const   express     = require('express'),
         router      = express.Router();
 //home route
 let transporter = nodemailer.createTransport({
-    service: 'ipage',
-    secure: false,
+    service: 'mail.ipage.com',
+    port: 587,
     auth: {
            user: mailPs.email,
            pass: mailPs.password
@@ -22,10 +22,38 @@ router.get('/home', isLogedIn, (req, res)=>{
     }
 });
 router.get('/register', (req, res)=>{
+    var data;
     if(!req.user){
+        var mailOptions = {
+            from: '"The Hacking School" <app@thehackingschool.com>', // sender address
+            to: 'erimadahmad@gmail.com', // list of receivers
+            subject: 'New application received from ', // Subject line
+            html: ' Hi'
+             
+          };
+          transporter.sendMail(mailOptions, function (err, info) {
+            if(err)
+              console.log(err)
+            else
+              console.log(info);
+         });
         res.render('register');
     }else{
-        res.redirect('/profile')
+        var mailOptions = {
+            from: '"The Hacking School" <erimadahmad@gmail.com>', // sender address
+            to: 'erimadahmad@gmail.com', // list of receivers
+            subject: 'New application received from '+data.name, // Subject line
+            html: ' <h1>'+data.name+'</h1><h3>Skills:</h3> <p>'+data.skills+'</p> <h3>1. Name:</h3> <p>'+data.name+'</p> <h3>2. Email:</h3><p>'+data.email+'</p><h3>3. Mobile:</h3><p>'+data.mobile+'</p> <h3>4. What do you currently do?</h3><p>'+data.designation+'</p><h3>5. Do you have background in coding?</h3><p>'+data.codingBackground+'</p><h3>6. Your objective of joining our bootcamp Career</h3><p>'+data.joiningObjective+'</p><h3>7. Which batch are you looking to join?</h3><p>'+data.batchType+'</p> <h3>8. How did you hear about us?</h3><p>'+data.source+'</p> <h3>9. What is your expectation from the program?</h3><p>'+data.expectations+'</p>'
+             
+          };
+          transporter.sendMail(mailOptions, function (err, info) {
+            if(err)
+              console.log(err)
+            else
+              console.log(info);
+         });
+         req.flash('success', '')
+        res.redirect('/success')
     }
 });
 router.get('/form', isLogedIn, (req, res)=>{
@@ -89,12 +117,6 @@ router.put('/form',isLogedIn, (req, res)=>{
                 html: ' <h1>'+data.name+'</h1><h3>Skills:</h3> <p>'+data.skills+'</p> <h3>1. Name:</h3> <p>'+data.name+'</p> <h3>2. Email:</h3><p>'+data.email+'</p><h3>3. Mobile:</h3><p>'+data.mobile+'</p> <h3>4. What do you currently do?</h3><p>'+data.designation+'</p><h3>5. Do you have background in coding?</h3><p>'+data.codingBackground+'</p><h3>6. Your objective of joining our bootcamp Career</h3><p>'+data.joiningObjective+'</p><h3>7. Which batch are you looking to join?</h3><p>'+data.batchType+'</p> <h3>8. How did you hear about us?</h3><p>'+data.source+'</p> <h3>9. What is your expectation from the program?</h3><p>'+data.expectations+'</p>'
                  
               };
-              transporter.sendMail(mailOptions, function (err, info) {
-                if(err)
-                  console.log(err)
-                else
-                  console.log(info);
-             });
               transporter.sendMail(mailOptions, function (err, info) {
                 if(err)
                   console.log(err)
