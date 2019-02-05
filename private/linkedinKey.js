5466b60718caef7ba5a498611c0e0da8d306609a
Imad Ahmed <erimadahmad@gmail.com>
2019-02-05 11:05:06 +0530

    v1

--- private/linkedinKey.js
+++ private/linkedinKey.js
@@ -1,7 +1,7 @@
 const linkedinKey = {
     clientId: '81gib89o1gn5y2',
     clientSecret: "ML0drXDAkZ5wq0Ok",
-    callbackURL: "https://thehackingschool.herokuapp.com/auth/linkedin/callback",
+    callbackURL: "http://schoolofcoding.in/auth/linkedin/callback",
 }
 
 module.exports = linkedinKey;
